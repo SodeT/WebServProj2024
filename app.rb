05a942e9db1c0a748aae3417354e4ef07f0f4612
SodeT <sode.timmeras@gmail.com>
2024-01-25 01:29:18 +0100

    Create, Read & Delete. 75% of CRUD

--- app.rb
+++ app.rb
@@ -1,83 +1,193 @@
+# The BIG Casino
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
 enable :sessions
-set :session_secret, 'random-key'
+# set :session_secret, 'random-key'
+
+permissions = {
+  'user': 0,
+  'creator': 1,
+  'admin': 2
+}
 
 def all_of(*paths)
-    return /(#{paths.join("|")})/
+  /(#{paths.join('|')})/
 end
 
 def open_db(path)
-    db = SQLite3::Database.new(path)
-    db.results_as_hash = true
-    return db
+  db = SQLite3::Database.new(path)
+  db.results_as_hash = true
+  db
 end
 
-before(all_of("/play", "/boosters", "/cards", "/events", "/prices")) do
-    db = open_db("db/db.sqlite3")
-    id = session[:id]
+before(all_of('/play', '/boosters', '/cards', '/events', '/prices')) do
+  db = open_db('db/db.sqlite3')
+  id = session[:id]
 
-    result = db.execute("SELECT COUNT(1) FROM users WHERE id = ?", id).first
-
-    if result == nil
-        redirect("/login")
-    end
+  result = db.execute('SELECT COUNT(1) FROM users WHERE id = ?', id).first
+  redirect('/login') if result.nil?
 end
 
-get('/')  do
-    slim(:home)
+get('/') do
+  slim(:home)
 end
 
-get('/signup') do 
-    slim(:signup)
+get('/signup') do
+  slim(:signup)
 end
 
 post('/signuppost') do
-    username = params[:username]
-    password = params[:password]
-    password_confirm = params[:password_confirm]
-    permissions = params[:permissions].to_i
-  
-    if password != password_confirm
-      return "Passwords don't match..."
-    end
-  
-    pwd_hash = BCrypt::Password.create(password)
-    db = open_db("db/db.sqlite3")
-    db.execute("INSERT INTO users (username, pwd_hash, permissions, tokens) VALUES (?,?,?,?)", username, pwd_hash, permissions, 0)
-    user = db.execute("SELECT * FROM users WHERE username = ?", username).first
-    session[:id] = user["id"]
-    session[:username] = user["username"]
+  username = params[:username]
+  password = params[:password]
+  password_confirm = params[:password_confirm]
+  permissions = params[:permissions].to_i
 
-    redirect("/play")
+  return "Passwords don't match..." if password != password_confirm
+
+  pwd_hash = BCrypt::Password.create(password)
+  db = open_db('db/db.sqlite3')
+  db.execute('INSERT INTO users (username, pwd_hash, permissions, tokens) VALUES (?,?,?,?)',
+             username, pwd_hash, permissions, 0)
+  user = db.execute('SELECT * FROM users WHERE username = ?', username).first
+  session[:id] = user['id']
+  session[:username] = user['username']
+
+  redirect('/play')
 end
-  
+
 get('/login') do
-    slim(:login)
+  slim(:login)
 end
-  
+
 post('/loginpost') do
-    db = open_db("db/db.sqlite3")
-    username = params[:username]
-    password = params[:password]
-  
-    user = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  
-    pwd_hash = user["pwd_hash"]
-  
-    if BCrypt::Password.new(pwd_hash) == password
-      session[:id] = user["id"]
-      session[:username] = user["username"]
-      redirect('/play')
-    else
-      "Fel lösenord..."
-    end
+  db = open_db('db/db.sqlite3')
+  username = params[:username]
+  password = params[:password]
+
+  user = db.execute('SELECT * FROM users WHERE username = ?', username).first
+
+  pwd_hash = user['pwd_hash']
+
+  if BCrypt::Password.new(pwd_hash) == password
+    session[:id] = user['id']
+    session[:username] = user['username']
+    redirect('/play')
+  else
+    'Fel lösenord...'
+  end
 end
 
 get('/play') do
-    slim(:play)
+  id = session['id']
+  db = open_db('db/db.sqlite3')
+  data = db.execute('SELECT username, tokens FROM users WHERE id = ?', id).first
+  slim(:play, locals: { user: data })
 end
 
+get('/boosters') do
+  db = open_db('db/db.sqlite3')
+  data = db.execute('SELECT * FROM boosters ORDER BY price')
+  slim(:boosters, locals: { boosters: data })
+end
+
+get('/cards') do
+  db = open_db('db/db.sqlite3')
+  data = db.execute('SELECT * FROM cards ORDER BY power')
+  slim(:cards, locals: { cards: data })
+end
+
+get('/events') do
+  db = open_db('db/db.sqlite3')
+  data = db.execute('SELECT * FROM events ORDER BY reward')
+  slim(:events, locals: { events: data })
+end
+
+get('/prices') do
+  db = open_db('db/db.sqlite3')
+  data = db.execute('SELECT * FROM prices ORDER BY value')
+  slim(:prices, locals: { prices: data })
+end
+
+get('/admin') do
+  db = open_db('db/db.sqlite3')
+  id = session[:id]
+
+  user = db.execute('SELECT permissions FROM users WHERE id = ?', id).first
+  redirect('/') if user['permission'] != permissions['admin']
+
+  boo = db.execute('SELECT * FROM boosters')
+  car = db.execute('SELECT * FROM cards')
+  eve = db.execute('SELECT * FROM events')
+  pri = db.execute('SELECT * FROM prices')
+
+  slim(:admin, locals: { boosters: boo, cards: car, events: eve, prices: pri })
+end
+
+post('/boosters/new') do
+  db = open_db('db/db.sqlite3')
+  name = params['name']
+  multiplier = params['multiplier'].to_i
+  price = params['price'].to_i
+  db.execute('INSERT INTO boosters (name, multiplier, price) VALUES (?, ?, ?)', name, multiplier, price)
+  redirect('/admin')
+end
+
+post('/boosters/:id/delete') do
+  db = open_db('db/db.sqlite3')
+  id = params[:id].to_i
+  db.execute('DELETE FROM boosters WHERE id = ?', id)
+  redirect('/admin')
+end
+
+post('/cards/new') do
+  db = open_db('db/db.sqlite3')
+  name = params['name']
+  power = params['power'].to_i
+  db.execute('INSERT INTO cards (name, power) VALUES (?, ?)', name, power)
+  redirect('/admin')
+end
+
+post('/cards/:id/delete') do
+  db = open_db('db/db.sqlite3')
+  id = params[:id].to_i
+  db.execute('DELETE FROM cards WHERE id = ?', id)
+  redirect('/admin')
+end
+
+post('/events/new') do
+  db = open_db('db/db.sqlite3')
+  name = params['name']
+  reward = params['reward'].to_i
+  condition = params['condition'].to_i
+  fee = params['fee'].to_i
+  db.execute('INSERT INTO events (name, reward, condition, fee) VALUES (?, ?, ?, ?)', name, reward, condition, fee)
+  redirect('/admin')
+end
+
+post('/events/:id/delete') do
+  db = open_db('db/db.sqlite3')
+  id = params[:id].to_i
+  db.execute('DELETE FROM events WHERE id = ?', id)
+  redirect('/admin')
+end
+
+post('/prices/new') do
+  db = open_db('db/db.sqlite3')
+  name = params['name']
+  value = params['value'].to_i
+  description = params['description']
+  db.execute('INSERT INTO prices (name, value, description) VALUES (?, ?, ?)', name, value, description)
+  redirect('/admin')
+end
+
+post('/prices/:id/delete') do
+  db = open_db('db/db.sqlite3')
+  id = params[:id].to_i
+  db.execute('DELETE FROM prices WHERE id = ?', id)
+  redirect('/admin')
+end
